19b5afaaae663d3798c47828a3b9499c4536f7b2
Darius Tohtin <dariustohtin@gmail.com>
2024-08-01 20:09:05 +0100

    task 3. Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,18 +38,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     Returns:
         A MySQLConnection object that was created use to manage the connection
     """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
     )
-    return connection
+    return db
 
 
 class RedactingFormatter(logging.Formatter):
