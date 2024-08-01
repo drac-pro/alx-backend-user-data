2d19c90820759d9b5a226386dd0bf152a3f8b6e5
Darius Tohtin <dariustohtin@gmail.com>
2024-08-01 19:50:09 +0100

    edit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,14 +38,17 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     Returns:
         A MySQLConnection object that was created use to manage the connection
     """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    connection = mysql.connector.connect(user=user,
-                                         password=passwd,
-                                         host=host,
-                                         database=db_name)
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
     return connection
 
 
