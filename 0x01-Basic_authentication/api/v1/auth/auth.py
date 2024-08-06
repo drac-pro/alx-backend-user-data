8a231e3e3dde9c3f7db36e573c179dd279cc8dc4
Darius Tohtin <dariustohtin@gmail.com>
2024-08-06 19:34:08 +0100

    task 3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/user/bin/env python3
+"""Module for API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Return:
+            False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Return:
+            None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Return:
+            None
+        """
+        return None
