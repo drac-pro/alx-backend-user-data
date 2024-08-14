c98d788192fa7d2ef19223931b39497e16bcd2b4
Darius Tohtin <dariustohtin@gmail.com>
2024-08-14 02:32:10 +0100

    task 0. User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" defines a class User for a database table named users
+using sqlalchemy
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ A user class
+    blueprint for a user object
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    sessiod_id = Column(String(250))
+    reset_token = Column(String(250))
