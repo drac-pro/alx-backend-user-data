47b31986c31a363b5fac6b5771fc39447041c1d5
Darius Tohtin <dariustohtin@gmail.com>
2024-08-14 16:32:25 +0100

    task 1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """creates and saves a new user to the database
 
