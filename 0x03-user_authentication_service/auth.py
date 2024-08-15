257faa945ead1ffd9823a4b5e58c0b1b9a149c02
Darius Tohtin <dariustohtin@gmail.com>
2024-08-15 04:11:31 +0100

    task 10. Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,18 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ creates and saves a user's session_id
+
+        Args:
+            email (str): user's email
+        Returns:
+            str: user's session_id
+        """
+        try:
+            user = self._db.find_user_by(**{'email': email})
+            self._db.update_user(user.id, session_id=_generate_uuid())
+            return user.session_id
+        except Exception:
+            pass
